9b48a60b2071bef61a00455f7541fd96900bb672
ohne-pixel <90521553+ohne-pixel@users.noreply.github.com>
2021-10-22 18:32:09 +0300

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -2,20 +2,17 @@
 
 using namespace std;
 
-int fact(int N)
-{
+int fact(int N){
     if  (N == 1)
         return 1;
     return N * fact(N-1);
 }
 
-int main()
-{
-    cout << "Calculating the factorial\n"
-        << "Enter the number:";
+int main() {
+    cout << "Calculating a factorial\n"
+        << "Input a number:";
     int a;
     cin >> a;
-
     if (a != 0) {
         cout << a << "!=" << fact(a) << endl;
     }
