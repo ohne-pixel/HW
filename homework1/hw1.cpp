75d37cd3e166866ce706681cbeb6f7c9c94ce82f
ohne-pixel <90521553+ohne-pixel@users.noreply.github.com>
2021-11-14 16:15:22 +0300

    Create hw1.cpp

---
+++ homework1/hw1.cpp
@@ -0,0 +1,34 @@
+#include <fstream>
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+
+int main(){
+    std::ifstream infile("in.txt");
+
+    int x, y;
+    int xl = 0, xr = 0, yl = 0, yr = 0;
+
+    double _cos_r, _cos_l;
+    _cos_l = 1; _cos_r = 1;
+
+    int x0, y0;
+    infile >> x0 >> y0;
+
+    while (infile >> x >> y){
+        double cos = (x0*x+y0*y) / (sqrt(x*x+y*y)*sqrt(x0*x0+y0*y0));
+        cos = std::lround(cos);
+
+        if (cos <= _cos_l && (y0*x < x0*y)){
+            _cos_l = cos;
+            xl = x; yl = y;
+        }
+
+        if (cos <= _cos_r && (y0*x >= x0*y)){
+            _cos_r = cos;
+            xr = x; yr = y;
+        }
+    }
+    std::cout << "Leftmost: " << xl << " " << yl << "\n";
+    std::cout << "Rightmost: " << xr << " " << yr << "\n";
+}
