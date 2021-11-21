7aba0e588091e89bc6f0e44ca74efb5dd1a4b757
ohne-pixel <90521553+ohne-pixel@users.noreply.github.com>
2021-11-21 20:44:32 +0300

    Create main.cpp

---
+++ homework2/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <fstream>
+#include <utility>
+#include <vector>
+#include <cmath>
+/* входной файл:
+h0
+vx vy
+x1 h1
+x2 h2
+...
+xn hn
+*/
+int main(int argc, char** argv) {
+    if (argc == 2) {
+        std::ifstream infile(argv[1]); // в argv[1] содержится строка с первым агрументом (имя файла)
+        std::vector < std::pair < double, double >> partition; // аналог std::cout << "1st argument: "<< argv[1] << std::endl для перегородки
+      
+        // идём по файлу
+        double h0;
+        double x = 0;
+        infile >> h0;
+
+        double vx, vy;
+        infile >> vx >> vy;
+
+        double g = -9.81;
+
+        int size = 0;
+        int interval = 0; // промежутки
+
+        while (true) {
+            int goal = (vx > 0) ? interval : interval - 1; // тернарный оператор
+
+            if (goal < 0) {
+                std::cout << "0" << std::endl;
+                return 0;
+            }
+          
+            if (goal > size - 1) {
+                double xi, hi; // положения перегородок
+              
+                if (infile >> xi >> hi) {
+                    partition.push_back(std::make_pair(xi, hi)); // добавление элементов в вектор перегородки 
+                    size++;
+                } else {
+                    std::cout << size << std::endl;
+                    return 0;
+                }
+            }
+          
+            // часть с расчётами
+            double t = std::abs((x-partition[goal].first) / vx);
+
+            x = partition[goal].first;
+            h0 = h0 + vy*t + 0.5*g*t*t;
+            vy = vy + g*t;
+
+            if (h0 <= 0) {
+                std::cout << interval << std::endl;
+                return 0;
+            }
+          
+            if (h0 > partition[goal].second) { // вектор скорости в обратную сторону
+                (vx > 0) ? interval++ : interval--;
+            } else {
+                vx = vx*(-1);
+            }
+        }
+    }
+}
